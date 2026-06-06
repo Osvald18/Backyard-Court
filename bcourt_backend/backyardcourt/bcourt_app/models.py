2522b1a00215a82106fa66baf21d10c3fbd14aff
Osvald18 <sabitouhhh@gmail.com>
2026-06-06 23:46:41 +0800

    added models on models.py

--- bcourt_backend/backyardcourt/bcourt_app/models.py
+++ bcourt_backend/backyardcourt/bcourt_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class patron(models.Model):
+    payment_id = models.IntegerField()
+    contact_number = models.BigIntegerField()
+    date = models.DateTimeField()
+    created_at = models.DateTimeField()
+
+
+class payment(models.Model):
+    total_payment = models.PositiveIntegerField()
+    screenshot = models.ImageField()
+
+
+
+
--- bcourt_backend/backyardcourt/bcourt_app/views.py
+++ bcourt_backend/backyardcourt/bcourt_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django import Httpresponse
 
 # Create your views here.
+def index(request):
+    return("Hello World, You are in the backyardcourt web application:)")
